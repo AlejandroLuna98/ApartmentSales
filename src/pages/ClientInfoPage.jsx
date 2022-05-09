8147e32ac52267a38e1c066a7e2ad1b975b67690
Alejandro Luna <alejandroluna0915@gmail.com>
2022-05-08 22:57:32 -0500

    client info page created

--- src/pages/ClientInfoPage.jsx
+++ src/pages/ClientInfoPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import { Form } from '../ui/components'
+import React, { useCallback, useState } from 'react';
+import { useButtonContext } from '../context/ButtonContext';
 
- const ClientInfoPage = () => {
+const ClientInfoPage = ({ nextButton }) => {
+  const { setButtonState } = useButtonContext();
+  const [username, setUsername] = useState('');
+
+  const validateUsername = useCallback(
+    (v) => {
+      const regex = /^[a-zA-Z_ ]*$/;
+
+      if (v && regex.test(v)) {
+        return setButtonState(false);
+      }
+      return setButtonState(true);
+    },
+    [setButtonState]
+  );
+  const handleUsername = useCallback(
+    (v) => {
+      const { value } = v.target;
+      setUsername(() => value);
+      validateUsername(value);
+    },
+    [setUsername, validateUsername]
+  );
   return (
-    <Form />
-  )
-}
-export default ClientInfoPage
+    <section>
+      <h1>Bienvenido</h1>
+      <p>
+        Vamos a registrar tu inmueble, por favor ingresa tu nombre y apellido
+      </p>
+      <input
+        type="text"
+        value={username}
+        onChange={handleUsername}
+        placeholder="Nombre y apellido"
+      />
+      {nextButton}
+    </section>
+  );
+};
+export default ClientInfoPage;
