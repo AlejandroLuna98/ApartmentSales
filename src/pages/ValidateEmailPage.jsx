e887f4e3d2c478f3d13fcec451b1d0b4d1b15ab8
Alejandro Luna <alejandroluna0915@gmail.com>
2022-05-08 22:56:22 -0500

    validate email page created

--- src/pages/ValidateEmailPage.jsx
+++ src/pages/ValidateEmailPage.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react';
+import { useButtonContext } from '../context/ButtonContext';
 
-const ValidateEmailPage = () => {
+const ValidateEmailPage = ({ nextButton }) => {
+  const { setButtonState } = useButtonContext();
+  const [emailValue, setEmailValue] = useState('');
+
+  const validateEmail = useCallback(
+    (v) => {
+      if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)) {
+        return setButtonState(false);
+      }
+      return setButtonState(true);
+    },
+    [setButtonState]
+  );
+
+  const handleOnchange = useCallback(
+    (e) => {
+      const { value } = e.target;
+      setEmailValue(value);
+      validateEmail(value);
+    },
+    [validateEmail]
+  );
+
   return (
-    <div>ValidateEmailPage</div>
-  )
-}
+    <section>
+      <h1>Correo Electronico</h1>
+      <p>Ingresa tu email para ponernos en contacto contigo</p>
+      <input
+        type="email"
+        value={emailValue}
+        onChange={handleOnchange}
+        placeholder="Correo electronico"
+      />
+      {nextButton}
+    </section>
+  );
+};
 export default ValidateEmailPage;
