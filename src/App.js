4d3677105be1737083fbe1269661920bf8ad420a
Alejandro Luna <alejandroluna0915@gmail.com>
2022-05-08 22:54:09 -0500

    render routes dynamicly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-undef */
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { useButtonContext } from './context/ButtonContext';
+
 import {
-  ApartmentAdressPage,
+  ApartmentAddressPage,
   ApartmentFeaturePage,
   ApartmentFloorPage,
   ClientInfoPage,
@@ -9,19 +14,76 @@ import {
   ValidateEmailPage,
 } from './pages';
 
+import data from './data/data.json';
+import { Stepper } from './components';
+
 function App() {
+  const [steps] = useState(data.steps || null);
+  const [currentStep, setCurrentStep] = useState({});
+  const [nextPath, setNextPath] = useState({});
+
+  const { setButtonState } = useButtonContext();
+
+  useEffect(() => {
+    if (steps) {
+      setCurrentStep(steps.find((item) => item.initial === true));
+    }
+  }, [steps]);
+
+  useEffect(() => {
+    const nextStep =
+      steps.find((item) => item.step === currentStep.step + 1) || currentStep;
+    setNextPath(nextStep);
+    setButtonState(true);
+  }, [currentStep, steps, setButtonState]);
+
+  const components = {
+    clientInfoPage: ClientInfoPage,
+    validateEmailPage: ValidateEmailPage,
+    apartmentAddressPage: ApartmentAddressPage,
+    apartmentFloorPage: ApartmentFloorPage,
+    apartmentFeaturePage: ApartmentFeaturePage,
+    summary: SummaryPage,
+  };
+  const handleNextStep = (nextStep) => {
+    setCurrentStep(steps.find((item) => item.step === nextStep.step));
+  };
+
+  const renderer = (config) => {
+    if (!steps) return <> </>;
+
+    return steps.map((step) => {
+      const Component = components[step.component];
+      return (
+        <React.Fragment key={step.step}>
+          <Route
+            key={step.step}
+            path={step.path}
+            element={
+              <Component
+                props={step}
+                nextButton={
+                  <Stepper
+                    nextStep={nextPath}
+                    handleNextStep={handleNextStep}
+                  />
+                }
+              />
+            }
+          />
+          {step.initial ? (
+            <Route path="/" element={<Navigate to={step.path} replace />} />
+          ) : (
+            <> </>
+          )}
+        </React.Fragment>
+      );
+    });
+  };
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={'datos-cliente'} element={<ClientInfoPage />} />
-        <Route path={'validacion-correo'} element={<ValidateEmailPage />} />
-        <Route path={'direccion'} element={<ApartmentAdressPage />} />
-        <Route path={'piso'} element={<ApartmentFloorPage />} />
-        <Route path={'caracteristicas'} element={<ApartmentFeaturePage />} />
-        <Route path={'resumen'} element={<SummaryPage />} />
-        <Route path={'/'} element={<ClientInfoPage />} />
-        <Route path={'*'}  element={<Navigate to="/" replace />} />
-      </Routes>
+      <Routes>{renderer()}</Routes>
     </BrowserRouter>
   );
 }
