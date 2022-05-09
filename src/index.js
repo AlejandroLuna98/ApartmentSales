ba865367798daa58fcedd5ef23b91ae0de1ea079
Alejandro Luna <alejandroluna0915@gmail.com>
2022-05-08 23:50:38 -0500

    context added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ButtonProvider } from './context/ButtonContext';
+import { SummaryProvider } from './context/SummaryContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ButtonProvider>
-      <App />
+      <SummaryProvider>
+        <App />
+      </SummaryProvider>
     </ButtonProvider>
   </React.StrictMode>
 );
